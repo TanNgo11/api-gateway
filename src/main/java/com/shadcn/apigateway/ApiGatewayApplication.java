8e8484e56d619681383c49cce57bf1bec664a897
Bui Hoang Huy <74142615+towh2301@users.noreply.github.com>
2024-08-10 21:44:27 +0700

    test push

--- src/main/java/com/shadcn/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/shadcn/apigateway/ApiGatewayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-
+		// tests
 		//test
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
