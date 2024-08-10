64c9e7821b6b2baeec81d0551944899da501a959
TanNgo11 <tan.ngo.cit20@eiu.edu.vn>
2024-08-10 16:15:19 +0700

    change-2

--- src/main/java/com/shadcn/apigateway/dto/request/IntrospectRequest.java
+++ src/main/java/com/shadcn/apigateway/dto/request/IntrospectRequest.java
@@ -11,9 +11,5 @@ import lombok.experimental.FieldDefaults;
 public class IntrospectRequest {
     //change to token
     //change 2
-    //change 3
-    //change 4
-    //change 5
-    //change 56
     String token;
 }
