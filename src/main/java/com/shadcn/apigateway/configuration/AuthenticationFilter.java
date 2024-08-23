99871c1eba51ee5c8b4bea76d51922ad48f07ee5
TanNgo11 <tan.ngo.cit20@eiu.edu.vn>
2024-08-23 09:26:12 +0700

    feat/update-module-notification

--- src/main/java/com/shadcn/apigateway/configuration/AuthenticationFilter.java
+++ src/main/java/com/shadcn/apigateway/configuration/AuthenticationFilter.java
@@ -7,6 +7,7 @@ import com.shadcn.apigateway.service.IdentityService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -14,12 +15,14 @@ import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -30,18 +33,45 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     IdentityService identityService;
     ObjectMapper objectMapper;
 
+    @NonFinal
+    private String[] publicEndpoints = {
+            "/identity/api/v1/auth/.*",
+            "/identity/api/v1/users/registration",
+            "/notification/email/send"
+    };
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Enter authentication filter....");
+
+        if (isPublicEndpoint(exchange.getRequest()))
+            return chain.filter(exchange);
+
+        log.info("Enter authentication filter....2");
 
         List<String> authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
         if (CollectionUtils.isEmpty(authHeader))
             return unauthenticated(exchange.getResponse());
-        return chain.filter(exchange);
+
+        String token = authHeader.get(0).replace("Bearer ", "");
+        log.info("Token: {}", token);
+
+        return identityService.introspect(token).flatMap(introspectResponse -> {
+            if (introspectResponse.getResult().isValid())
+                return chain.filter(exchange);
+            else
+                return unauthenticated(exchange.getResponse());
+        }).onErrorResume(throwable -> unauthenticated(exchange.getResponse()));
     }
 
     @Override
     public int getOrder() {
-        return 0;
+        return -1;
+    }
+
+    private boolean isPublicEndpoint(ServerHttpRequest request) {
+        return Arrays.stream(publicEndpoints)
+                .anyMatch(s -> request.getURI().getPath().matches(s));
     }
 
     Mono<Void> unauthenticated(ServerHttpResponse response) {
